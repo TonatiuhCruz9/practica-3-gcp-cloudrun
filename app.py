ce8886fd616c3c85213e3c12477e136829ab6a74
TonatiuhCruz9 <36651318+TonatiuhCruz9@users.noreply.github.com>
2025-11-06 20:01:12 -0600

    Update app.py

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ def hello() -> str:
     # https://cloud.google.com/run/docs/logging#correlate-logs
     logger.info("Child logger with trace Id.")
 
-    return "Hello, World from UP Tona here, let's go to Silicon Valley!"
+    return "Hello, World!"
 
 
 def shutdown_handler(signal_int: int, frame: FrameType) -> None:
